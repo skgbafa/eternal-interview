1411ba039152e931c58149eeed831eda26241130
Sam Gbafa <skgbafa@gmail.com>
2021-12-28 18:31:11 -0500

    cleanup index

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,31 +1,9 @@
 import { ApolloServer, gql} from 'apollo-server-express';
-import {
-  ApolloServerPluginLandingPageGraphQLPlayground
-} from 'apollo-server-core';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
   
 import schema  from './schema';
-// import { typeDefs } from "./graphql/schema";
-// import { resolvers } from "./graphql/resolvers";
-
 
 export default async (app: any, context: any) => {
-
-  // Construct a schema, using GraphQL schema language
-  const typeDefs = gql`
-    type Query {
-    hello: String
-    }
-    `;
-
-  // Provide resolver functions for your schema fields
-  const resolvers = {
-    Query: {
-      hello: () => 'Hello world!',
-    },
-  };
-    // const server = new ApolloServer({ typeDefs, resolvers });
-
-  // server.applyMiddleware({ app });
 
   const server = new ApolloServer({
     // context: ({ req }: any) => {
@@ -45,7 +23,4 @@ export default async (app: any, context: any) => {
     app,
     path: '/graphql',
   });
-
-  return ;
-    
 };
